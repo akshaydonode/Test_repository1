4563dc9588bd617800d5c654109042063c9443e2
Donode <akshay.donode@capgemini.com>
2019-03-05 10:51:31 +0530

    Added division() function

--- calculator.java
+++ calculator.java
@@ -5,7 +5,13 @@ public class calculator{
 			System.out.println("calculator java program");
 
 		}
-publiv static void addition(int num1,int num2){
+public static void addition(int num1,int num2){
 			System.out.println("Addition="+(num1+num2));
+}
+public static void division(int num1,int num2){
+		if(num2==0)
+			System.out.println("Divisor should be greater than 0");
+		else
+			System.out.println("Division="+(num1/num2));
 }
 }
